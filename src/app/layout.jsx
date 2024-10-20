831472bd23b228bc3698e3e206da92657f4df0cf
Erison <erison7596e@gmail.com>
2024-10-20 20:19:10 -0300

    alterado favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ChatBot",
+  title: "ChatBotSNCT",
   description: "Chatbot built with Rasa and React",
 };
 
@@ -16,6 +15,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
